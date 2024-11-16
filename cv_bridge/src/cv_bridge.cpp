b47289448f7a741c91e55f8bd1e0f55199dbb99c
ijnek <kenjibrameld@gmail.com>
2024-11-16 16:41:02 +0000

    ensure same format encoding happens for all encoded types
    (cherry picked from commit e7cfc5dc6ce4ba5981b6ce5c8177f114999c88e9)
    
    # Conflicts:
    #	cv_bridge/src/cv_bridge.cpp

--- cv_bridge/src/cv_bridge.cpp
+++ cv_bridge/src/cv_bridge.cpp
@@ -121,7 +121,7 @@ int getCvType(const std::string & encoding)
 /// @cond DOXYGEN_IGNORE
 
 enum Encoding { INVALID = -1, GRAY = 0, RGB, BGR, RGBA, BGRA, YUV422, YUV422_YUY2, BAYER_RGGB, BAYER_BGGR,
-  BAYER_GBRG, BAYER_GRBG};
+  BAYER_GBRG, BAYER_GRBG, MAX_ENCODING};
 
 Encoding getEncoding(const std::string & encoding)
 {
@@ -151,7 +151,7 @@ static const int SAME_FORMAT = -1;
 std::map<std::pair<Encoding, Encoding>, std::vector<int>> getConversionCodes()
 {
   std::map<std::pair<Encoding, Encoding>, std::vector<int>> res;
-  for (int i = 0; i <= 5; ++i) {
+  for (int i = 0; i < MAX_ENCODING; ++i) {
     res[std::pair<Encoding, Encoding>(Encoding(i), Encoding(i))].push_back(SAME_FORMAT);
   }
 
@@ -599,7 +599,12 @@ CvImageConstPtr cvtColorForDisplay(
       } else {
         // We choose BGR by default here as we assume people will use OpenCV
         if ((enc::bitDepth(source->encoding) == 8) ||
+<<<<<<< HEAD
           (enc::bitDepth(source->encoding) == 16))
+=======
+          (enc::bitDepth(source->encoding) == 16) ||
+          (enc::bitDepth(source->encoding) == 32))
+>>>>>>> e7cfc5d (ensure same format encoding happens for all encoded types)
         {
           encoding = enc::BGR8;
         } else {
